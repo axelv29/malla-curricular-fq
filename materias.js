930c14ec692a4143115784dd1f0658d3c8f048cc
axelv29 <52230319+axelv29@users.noreply.github.com>
2025-08-04 16:21:19 -0300

    Create materias.js

---
+++ materias.js
@@ -0,0 +1,17 @@
+const materias = [
+  {
+    id: "MAT01",
+    nombre: "Matemática 01",
+    creditos: 14,
+    semestre: 1,
+    pre: []
+  },
+  {
+    id: "QUI01",
+    nombre: "Química General I",
+    creditos: 7,
+    semestre: 1,
+    pre: []
+  },
+  // Y así para todas
+];
